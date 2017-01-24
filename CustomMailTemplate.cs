aabb1f0854b8bd78eacce40ac3fb4512aae28d55
Christian van de Luitgaarden <c.vandeluitgaarden@outlook.com>
2017-01-24 13:50:17 +0100

    Absolutify
    Added Absolutify method to make retalive urls in the mail template absolute.

--- CustomMailTemplate.cs
+++ CustomMailTemplate.cs
@@ -1,14 +1,11 @@
 namespace Estate.Framework.WFFM.Pipelines.ProcessMessage
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Web;
-    using Sitecore.WFFM.Abstractions.Mail;
-    using Sitecore.WFFM.Abstractions.Actions;
     using System.IO;
+    using Sitecore.Form.Core.Pipelines.ProcessMessage;
+    using HtmlAgilityPack;
+    using Sitecore;
 
     public class CustomMailTemplate
     {
@@ -29,12 +26,46 @@ namespace Estate.Framework.WFFM.Pipelines.ProcessMessage
                     if (template.Contains(Token))
                     {
                         var body = template.Replace(Token, args.Mail.ToString());
-
+                        body = Absolutify(body);
                         args.IsBodyHtml = true;
                         args.Mail.Clear();
                         args.Mail.Append(body);
                     }
                 }
+            }
+        }
+
+        private string Absolutify(string body)
+        {
+            var html = new HtmlDocument();
+            html.LoadHtml(body);
+
+            foreach(HtmlNode node in html.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                string src = node.GetAttributeValue("src", string.Empty);
+                Absolutify(src, node, "src");
+            }
+
+            foreach (HtmlNode node in html.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                string href = node.GetAttributeValue("href", string.Empty);
+                Absolutify(href, node, "href");
+            }
+
+            return html.DocumentNode.OuterHtml;
+        }
+
+        private void Absolutify(string href,  HtmlNode node, string attributeName)
+        {
+            var uri = new Uri(href, UriKind.RelativeOrAbsolute);
+            if (!uri.IsAbsoluteUri)
+            {
+
+                string url = StringUtil.EnsurePrefix('/', uri.OriginalString);
+                UriBuilder builder = new UriBuilder(HttpContext.Current.Request.Url);
+                builder.Path = uri.OriginalString;
+                uri = builder.Uri;
+                node.SetAttributeValue(attributeName, uri.ToString());
             }
         }
     }
